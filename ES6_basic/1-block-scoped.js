b54657e1c2b9d4b3332b0217e1dfb83b3f6b197c
essioui <salahessioui@gmail.com>
2024-07-20 15:58:05 +0100

    scope block

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    console.log(`Task is: ${task}, Task2 is: ${task2}`);
   }
 
   return [task, task2];
