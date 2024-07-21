2c119faf329fdbaffa3b334abb2763b3ec82cd43
essioui <salahessioui@gmail.com>
2024-07-21 17:34:39 +0100

    export

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,11 @@
+export class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
