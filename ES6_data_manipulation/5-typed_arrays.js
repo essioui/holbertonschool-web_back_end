3474bbb48f63ed5ab7e9f91dea56a56d02220adf
essioui <salahessioui@gmail.com>
2024-07-23 01:09:50 +0100

    Arrays of types

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,16 @@
 function createInt8TypedArray(length, position, value) {
+    
   const buffer = new ArrayBuffer(length);
 
-  const int8Array = new Int8Array(buffer);
+  const view = new DataView(buffer);
 
   if (position >= length || position < 0) {
-    throw new Error('position outside range');
+    throw new Error('Position outside range');
   }
 
-  int8Array[position] = value;
+  view.setInt8(position, value);
 
-  return buffer;
+  return view;
 }
 
 module.exports = createInt8TypedArray;
