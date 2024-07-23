3b605df2d9ab8bd971a66182cb42d24046b23f43
essioui <salahessioui@gmail.com>
2024-07-23 01:39:17 +0100

    startString

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,9 @@
 function cleanSet(set, startString) {
+
+  if (typeof startString !== 'string') {
+    return '';
+  }
+
   return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
