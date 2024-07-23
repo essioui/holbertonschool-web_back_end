e9f5f320fd059f10610fd8b8ac607c89eca12232
essioui <salahessioui@gmail.com>
2024-07-23 19:09:54 +0100

    promise boolean

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('API is not working'));
+    }
+  });
+}
+
+module.exports = getFullResponseFromAPI;
