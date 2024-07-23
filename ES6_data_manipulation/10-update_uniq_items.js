3f357b1095426baf22fbde875a3ed82305819a90
essioui <salahessioui@gmail.com>
2024-07-23 16:29:17 +0100

    updated map for all items with initial quantity at 1.

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,15 +1,23 @@
 function updateUniqueItems(map) {
+
   if (!(map instanceof Map)) {
-    throw new Error('is not map');
+
+    throw new Error('Cannot process');
+
   }
 
   for (const [key, value] of map) {
+
     if (value === 1) {
+
       map.set(key, 100);
+
     }
+
   }
 
   return map;
+  
 }
 
 module.exports = updateUniqueItems;
