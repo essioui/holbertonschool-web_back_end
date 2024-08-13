129fdc30605c6fee7942effcb3d7a9d7c4b3a953
essioui <salahessioui@gmail.com>
2024-08-13 18:27:19 +0100

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import math
+"""
+2-floor.py
+this module use library math for back integer number of float n
+"""
+
+def floor(n: float) -> int:
+    """
+    function floor has one argument n
+    n: type float
+    return nearest integer of n by used math
+    """
+    return math.floor(n)
