963b143155e550f528a6b083550f93ad78f0774e
essioui <salahessioui@gmail.com>
2024-08-13 18:06:20 +0100

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+"""
+0-add.py
+
+This module contains the `add` function that adds two decimal numbers and returns the result.
+"""
 def add(a: float, b: float) -> float:
+    """
+    function add a type-annotated have two arguments
+    a: type float
+    b: type float
+    return sum a + b  type float
+    """
     return a + b
