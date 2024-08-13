f4d42c00a1f983a9e32d7206169493f0de2250c4
essioui <salahessioui@gmail.com>
2024-08-13 18:17:38 +0100

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+1-concat.py
+
+This module contains the `concat` function that concat str1 and str2.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function concat a type-annotated have two arguments
+    str1: type str
+    str2: type str
+    return concat str1 + str2  type str
+    """
+    return str1 + str2
