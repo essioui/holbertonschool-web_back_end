7ffd3d13afc15901ebcc72f2b0aad055fc05822f
essioui <salahessioui@gmail.com>
2024-08-13 19:24:15 +0100

    Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+3-to_str.py
+this module contains the `to_str`  return string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function to_str have one argument
+    n: float
+    return string of the float
+    """
+    return str(n)
