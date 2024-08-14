e51d7d4273afbe48e73ec82c70452edeb3e58615
essioui <salahessioui@gmail.com>
2024-08-14 23:06:15 +0100

    concurrent_coroutines

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
-"""Execute multiple coroutines"""
+"""
+this module import 'wait_random' from other file for display list of time wait
+"""
+
 
 from typing import List
-
-wait_random = __import__("0-basic_async_syntax").wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Async waits for 'n' random delays, return sorted list."""
+    """
+    function return list 
+    """
     delay_list = []
-    for i in range(n):
+    for x in range(n):
         delay = await wait_random(max_delay)
         delay_list.append(delay)
     return sorted(delay_list)
