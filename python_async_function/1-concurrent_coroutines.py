43ec4e7480d759747575d5d594d8102ebd91d05e
essioui <salahessioui@gmail.com>
2024-08-17 10:53:43 +0100

    corountines

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,7 @@ this module import 'wait_random' from other file for display list of time wait
 
 
 from typing import List
+import asyncio
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -14,8 +15,9 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     function return list 
     """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     delay_list = []
-    for x in range(n):
-        delay = await wait_random(max_delay)
-        delay_list.append(delay)
-    return sorted(delay_list)
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delay_list.append(task)
+    return delay_list
