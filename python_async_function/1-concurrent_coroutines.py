40614c396d02c517d9520e38e9781d39b8f0b2c5
essioui <salahessioui@gmail.com>
2024-08-17 11:23:59 +0100

    execute multiple coroutines

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    function return list 
+    Return a list of delays in ascending order.
+
+    Args:
+        n (int): n times to call wait_random.
+        max_delay (int): The maximum delay value.
+
+    Returns:
+        List[float]: A list of delay times
     """
     delay_list = []
     for x in range(n):
