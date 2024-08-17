664c1e7b93df5bb26a986b9d9e555b40b73ddca8
essioui <salahessioui@gmail.com>
2024-08-17 11:02:45 +0100

    pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 """
-this module import 'wait_random' from other file for display list of time wait
+This module imports 'wait_random' from another file to display a list of wait times.
 """
-
 
 from typing import List
 
-
 wait_random = __import__('0-basic_async_syntax').wait_random
-
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    function return list 
+    Return a list of delays in ascending order.
+
+    Args:
+        n (int): The number of times to call wait_random.
+        max_delay (int): The maximum delay value.
+
+    Returns:
+        List[float]: A list of delay times in ascending order.
     """
     delay_list = []
-    for x in range(n):
+    for _ in range(n):
         delay = await wait_random(max_delay)
         delay_list.append(delay)
     return sorted(delay_list)
