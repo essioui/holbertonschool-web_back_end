d855038b4739455d77fe66b20d6ee3699cd271cb
essioui <salahessioui@gmail.com>
2024-08-20 23:39:49 +0100

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -19,6 +19,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     Yields:
         float: A random number between 0 and 10 after a 1-second delay.
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
