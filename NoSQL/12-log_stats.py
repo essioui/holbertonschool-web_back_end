180527023a99c5a3b27b6244c1f8cc3e02626c85
essioui <salahessioui@gmail.com>
2024-08-25 12:40:10 +0100

    dump.zip

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-"""Print info f nginx logs """
+"""Print info from nginx logs"""
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """ check for all elements in a collection """
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
+    # عدد المستندات في المجموعة
     print(f"{collection.estimated_document_count()} logs")
 
+    # حساب عدد المستندات لكل نوع من الطرق
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
         method_count = collection.count_documents({'method': method})
         print(f"\tmethod {method}: {method_count}")
 
-    check_get = collection.count_documents(
-        {'method': 'GET', 'path': "/status"})
+    # حساب عدد المستندات التي تحتوي على method=GET و path=/status
+    check_get = collection.count_documents({'method': 'GET', 'path': "/status"})
     print(f"{check_get} status check")
