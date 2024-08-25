be6ccf92c489e6f20b164d43d1089104bfc9f888
essioui <salahessioui@gmail.com>
2024-08-25 11:54:21 +0100

    insert new document

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 """
+9-insert_school.py function inserts a new document
 """
 
 
 def insert_school(mongo_collection, **kwargs):
     """
+    Inserts a new document in the collection
+    Args:
+        mongo_collection: collection object.
+        **kwargs (Any): Key-value
+    return: the new _id
     """
     return mongo_collection.insert_one(kwargs).inserted_id
