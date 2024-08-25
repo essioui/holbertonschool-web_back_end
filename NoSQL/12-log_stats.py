ceaa384ac7fb19a7c8fb6edbc6931ff5e762362b
essioui <salahessioui@gmail.com>
2024-08-25 12:25:59 +0100

    Nginx

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,17 +4,21 @@
 """
 from pymongo import MongoClient
 
-if __name__ == "__main__":
+
+def main():
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
-
-    print(f"{collection.estimated_document_count()} logs")
-
+    total_logs = collection.estimated_document_count()
+    print(f"{total_logs} logs")
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
         method_count = collection.count_documents({'method': method})
         print(f"\tmethod {method}: {method_count}")
-
-    check_get = collection.count_documents(
+    status_check = collection.count_documents(
         {'method': 'GET', 'path': "/status"})
-    print(f"{check_get} status check")
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    main()
