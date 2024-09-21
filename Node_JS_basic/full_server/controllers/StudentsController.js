81581b94ab9c68981cc5d508e49a45a23b713d31
essioui <salahessioui@gmail.com>
2024-09-21 14:59:41 +0100

    Major parameter

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -11,6 +11,7 @@ export default class StudentsController {
       });
       res.status(200).send(responseText.trim());
     } catch (error) {
+      console.error('Error loading database:', error);
       res.status(500).send('Cannot load the database');
     }
   }
